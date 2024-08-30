a77b16036400a13c0f81d211c91a35c38fa410bc
A1tura <plandik275@gmail.com>
2024-08-30 16:39:14 +0200

    temp fix: types

--- dal/blog.dal.ts
+++ dal/blog.dal.ts
@@ -1,23 +1,26 @@
 import { Collection, ObjectId } from "mongodb";
-import {db} from "../db/db";
+import { db } from "../db/db";
 
-import {Blog as blogPostType} from "../types/blog.type";
+import { Blog as blogPostType } from "../types/blog.type";
 
 const getCollection = (): Collection<blogPostType.BlogPostDB> => db.collection("blog-posts");
 
 export class BlogPost {
     blogPost?: blogPostType.BlogPost;
 
-    constructor(blogPost: blogPostType.BlogPost) {
+    constructor(blogPost?: blogPostType.BlogPost) {
         this.blogPost = blogPost;
     }
 
     async create(): Promise<void> {
-        await getCollection().insertOne(this.blogPost);
+        if (this.blogPost) {
+            //@ts-ignore
+            await getCollection().insertOne(this.blogPost);
+        }
     }
 
     async deletePost(_id: ObjectId): Promise<void> {
-        await getCollection().deleteOne({_id});
+        await getCollection().deleteOne({ _id });
     }
 
     async getPosts(): Promise<blogPostType.BlogPostDB[]> {
