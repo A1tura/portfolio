5bbb0e51b71cea37930c16f37550ba3694f63bb5
A1tura <plandik275@gmail.com>
2024-08-30 16:29:29 +0200

    implemented: block public dal

--- dal/blog.dal.ts
+++ dal/blog.dal.ts
@@ -1,12 +1,12 @@
-import { Collection } from "mongodb";
+import { Collection, ObjectId } from "mongodb";
 import {db} from "../db/db";
 
 import {Blog as blogPostType} from "../types/blog.type";
 
-const getCollection = (): Collection => db.collection("blog-posts");
+const getCollection = (): Collection<blogPostType.BlogPostDB> => db.collection("blog-posts");
 
 export class BlogPost {
-    blogPost: blogPostType.BlogPost;
+    blogPost?: blogPostType.BlogPost;
 
     constructor(blogPost: blogPostType.BlogPost) {
         this.blogPost = blogPost;
@@ -14,5 +14,16 @@ export class BlogPost {
 
     async create(): Promise<void> {
         await getCollection().insertOne(this.blogPost);
+    }
+
+    async deletePost(_id: ObjectId): Promise<void> {
+        await getCollection().deleteOne({_id});
+    }
+
+    async getPosts(): Promise<blogPostType.BlogPostDB[]> {
+        const cursor = await getCollection().find();
+        const document = await cursor.toArray();
+
+        return document;
     }
 }
